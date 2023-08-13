6caf42f5658a577d61c3c5a7cadde3d433d139b3
Sergey Kachaev <metallian@yandex.ru>
2023-08-13 13:14:20 +0300

    few things more work - user page mostly

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/MainController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/MainController.java
@@ -3,6 +3,7 @@ package ru.kata.spring.boot_security.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
@@ -19,13 +20,28 @@ public class MainController {
     }
 
     @GetMapping("/")
-    public String homePage (){
-        return "home";
+    public ModelAndView homePage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        return modelAndView;
     }
 
-    @GetMapping("/authenticated")
-    public String pageForAuthenticatedUser (Principal principal) {
+
+    @GetMapping("/user")
+    public ModelAndView modelAndView (Principal principal) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("user");
         User user = userService.findByUsername(principal.getName());
-        return "secured part of web service " + user.getUsername() + ", " + user.getEmail();
+        modelAndView.addObject(user);
+        return modelAndView;
     }
+
+
+
+//
+//    @GetMapping("/authenticated")
+//    public String pageForAuthenticatedUser(Principal principal) {
+//        User user = userService.findByUsername(principal.getName());
+//        return "secured part of web service " + user.getUsername() + ", " + user.getEmail();
+//    }
 }
